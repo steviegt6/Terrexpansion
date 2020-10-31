7498c592910d5b5738a8844d9bf1d381af429312
Stevie <xxlennygamerxx@gmail.com>
2020-10-31 14:49:02 -0700

    More descriptive SpawnNPCCommand

--- Content/Commands/SpawnNPCCommand.cs
+++ Content/Commands/SpawnNPCCommand.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terrexpansion.Common.Configs.ServerSide;
@@ -11,31 +12,51 @@ namespace Terrexpansion.Content.Commands
 
         public override CommandType Type => CommandType.Chat;
 
+        public override string Usage => "/summonnpc <ID> (<ID> can be NPCID.____, ____ without the NPCID., or an integer.";
+
+        public override string Description => "Allows you to forcefully spawn an NPC. Only usable with testing mode enabled.";
+
         public override void Action(CommandCaller caller, string input, string[] args)
         {
-            string realInput = args[0];
             if (ModContent.GetInstance<ServerSideConfig>().testingMode)
             {
-                if (realInput.StartsWith("NPCID."))
+                if (args[0].StartsWith("NPCID."))
                 {
-                    string[] output = realInput.Split('.');
+                    string[] output = args[0].Split('.');
                     if (NPCID.Search.TryGetId(output[1], out int id))
                     {
                         NPC.NewNPC((int)caller.Player.position.X, (int)caller.Player.position.Y + 100, id);
                         return;
                     }
+                    else
+                    {
+                        Main.NewText($"No NPCID by the name of {output[1]} found!", Color.Red);
+                    }
                 }
-                else if (char.IsLetter(realInput[0]))
+                else if (char.IsLetter(args[0][0]))
                 {
-                    if (NPCID.Search.TryGetId(realInput, out int id))
+                    if (NPCID.Search.TryGetId(args[0], out int id))
                     {
                         NPC.NewNPC((int)caller.Player.position.X, (int)caller.Player.position.Y + 100, id);
                         return;
                     }
+                    else
+                    {
+                        Main.NewText($"No NPC with an NPCID name of {args[0]} found!", Color.Red);
+                    }
                 }
                 else
                 {
-                    NPC.NewNPC((int)caller.Player.position.X, (int)caller.Player.position.Y + 100, int.Parse(realInput));
+                    int id = int.Parse(args[0]);
+
+                    if (id >= NPCID.NegativeIDCount && id <= NPCLoader.NPCCount)
+                    {
+                        NPC.NewNPC((int)caller.Player.position.X, (int)caller.Player.position.Y + 100, id);
+                    }
+                    else
+                    {
+                        Main.NewText($"{id} was either smaller than or equal to {NPCID.NegativeIDCount} or larger than or equal to {NPCLoader.NPCCount}", Color.Red);
+                    }
                 }
             }
         }
