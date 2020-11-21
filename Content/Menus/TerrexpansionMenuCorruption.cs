004b2cace6cd6fcd90a69674648bc3c63e8efafc
Stevie <xxlennygamerxx@gmail.com>
2020-11-21 02:30:39 -0800

    Override Logo

--- Content/Menus/TerrexpansionMenuCorruption.cs
+++ Content/Menus/TerrexpansionMenuCorruption.cs
@@ -1,7 +1,13 @@
-﻿namespace Terrexpansion.Content.Menus
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace Terrexpansion.Content.Menus
 {
     public class TerrexpansionMenuCorruption : StaticSplashTextMenu
     {
         public override string DisplayName => "Terrexpansion Corruption";
+
+        public override Asset<Texture2D> Logo => ModContent.GetTexture("Terrexpansion/Assets/TerrexpansionLogoCorruption");
     }
 }
--- Content/Menus/TerrexpansionMenuCrimson.cs
+++ Content/Menus/TerrexpansionMenuCrimson.cs
@@ -1,7 +1,13 @@
-﻿namespace Terrexpansion.Content.Menus
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+
+namespace Terrexpansion.Content.Menus
 {
     public class TerrexpansionMenuCrimson : StaticSplashTextMenu
     {
         public override string DisplayName => "Terrexpansion Crimson";
+
+        public override Asset<Texture2D> Logo => ModContent.GetTexture("Terrexpansion/Assets/TerrexpansionLogoCrimson");
     }
 }
